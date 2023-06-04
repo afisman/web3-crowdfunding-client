4720b25da3e271d663a0077097ae5c41794d70ba
afisman <afisman@gmail.com>
2023-06-04 18:23:39 +0200

    worked on campaign details page

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -50,13 +50,23 @@ export const StateContextProvider = ({ children }) => {
         return parsedCampaigns;
     }
 
+    const getUserCampaigns = async () => {
+
+        const allCampaigns = await getCampaigns();
+
+        const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+        return filteredCampaigns;
+    }
+
     return <StateContext.Provider
         value={{
             address,
             contract,
             connect,
             createCampaign: publishCampaign,
-            getCampaigns
+            getCampaigns,
+            getUserCampaigns
         }}
     >
         {children}
